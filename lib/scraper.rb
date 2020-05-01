d04864c0f9ecccd6b97057d8a1a315f7a8515f5a
Nabil Hayet <nabil.bracu@gmail.com>
2020-05-01 10:26:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ class Scraper
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     doc.css(".post").first.css("h2").text
     doc.css(".post").first.css(".date").text
-    
+    doc.css(".post").first.css("p").text
     
     binding.pry 
   end 
